13634e470432863d237b61e33ef7d74886c42569
Flaviohmm <fhenrique609@gmail.com>
2021-06-18 20:14:35 -0300

    Criação do index

---
+++ src/pages/encontrar-diarista.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const EncontrarDiaristas: React.FC = () => {
+  return (
+    <View>
+      <Text>Encontrar Diarista</Text>
+    </View>
+  );
+};
+
+export default EncontrarDiaristas;
