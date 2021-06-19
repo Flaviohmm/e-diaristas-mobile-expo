2085848a7f3f9e33acc25fb6939def829757b5e0
Flaviohmm <fhenrique609@gmail.com>
2021-06-18 21:16:17 -0300

    Button

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import { Text, View } from "react-native";
+import Button from "ui/components/inputs/Button/Button";
 
 const Index: React.FC = () => {
   return (
-    <View>
-      <Text>Index</Text>
+    <View style={{ flex: 1, justifyContent: "center" }}>
+      <Button mode={"contained"} onPress={() => {}}>
+        Encontrar Diarista
+      </Button>
     </View>
   );
 };
