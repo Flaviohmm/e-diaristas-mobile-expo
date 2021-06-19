1063caeb42b2aad7c62a2278f295d66129a4c7ea
Flaviohmm <fhenrique609@gmail.com>
2021-06-18 23:51:37 -0300

    PageTitle and TextInput

--- src/pages/encontrar-diarista.tsx
+++ src/pages/encontrar-diarista.tsx
@@ -1,10 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import { Text, View } from "react-native";
+import PageTitle from "ui/components/data-display/PageTitle/PageTitle";
+import TextInput from "ui/components/inputs/TextInput/TextInput";
+import { TextInputMask } from "react-native-masked-text";
+import Button from "ui/components/inputs/Button/Button";
 
 const EncontrarDiaristas: React.FC = () => {
+  const [cep, setCep] = useState("");
+
   return (
     <View>
-      <Text>Encontrar Diarista</Text>
+      <PageTitle
+        title={"Conheça os profissionais"}
+        subtitle={
+          "Preencha seu endereço e veja todos os profissionais da sua localidade"
+        }
+      />
+
+      <TextInputMask
+        value={cep}
+        onChangeText={setCep}
+        type={"custom"}
+        options={{
+          mask: "99.999-999",
+        }}
+        customTextInput={TextInput}
+        customTextInputProps={{
+          label: "Digite seu CEP",
+        }}
+      />
+
+      <Button mode={"contained"} style={{ marginTop: 32 }}>
+        Buscar
+      </Button>
     </View>
   );
 };
